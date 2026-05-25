8dcc5f64336bd2c10d3ec85070d2a528245a7a11
jitsmep <pjoshuva31@gmail.com>
2026-05-25 22:28:04 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // This tells the compiler to look for active light/dark classes
+    darkMode: 'class',
+  }
 };
 
 export default nextConfig;
