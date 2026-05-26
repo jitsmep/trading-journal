170d56aef5cf39ced8581ca485ef9a9568d5da4d
jitsmep <pjoshuva31@gmail.com>
2026-05-26 15:26:46 +0530

    Delete src/components/TradesProviderWrapper.tsx

--- src/components/TradesProviderWrapper.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { TradesProvider } from "@/context/TradesContext";
-
-export function TradesProviderWrapper({ children }: { children: React.ReactNode }) {
-  return <TradesProvider>{children}</TradesProvider>;
-}
